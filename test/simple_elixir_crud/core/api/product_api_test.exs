42cbe711446204040b98b3ae24ae5bfba1a7c643
Samyr Ribeiro <saamyr@live.com>
2024-01-02 11:30:47 -0300

    tests: finished tests with describe and setup

--- test/simple_elixir_crud/core/api/product_api_test.exs
+++ test/simple_elixir_crud/core/api/product_api_test.exs
@@ -3,23 +3,38 @@ defmodule SimpleElixirCrud.Core.Product.ApiTest do
 
   alias SimpleElixirCrud.Core.Product
 
-  test "Get all products" do
-    assert {:ok, []} = Product.Api.all()
-  end
+  describe "Products API" do
+    setup do
+      params = %{
+        title: "um produto",
+        description: "Meu primeiro produto"
+      }
 
-  test "Insert one product" do
-    params = %{
-      title: "um produto",
-      description: "Meu primeiro produto"
-    }
+      {:ok, sucess_params: params}
+    end
 
-    assert {:ok,
-            %Product{
-              title: title,
-              description: description
-            }} = Product.Api.insert(params)
+    test "Get all products" do
+      assert {:ok, []} = Product.Api.all()
+    end
 
-    assert ^title = params.title
-    assert ^description = params.description
+    test "Insert one product", context do
+      assert {:ok,
+              %Product{
+                title: title,
+                description: description
+              }} = Product.Api.insert(context.sucess_params)
+
+      assert ^title = context.sucess_params.title
+      assert ^description = context.sucess_params.description
+    end
+
+    test "Get all products afeter inster", context do
+      for _i <- 1..10 do
+        assert {:ok, _} = Product.Api.insert(context.sucess_params)
+      end
+
+      assert {:ok, items} = Product.Api.all()
+      assert 10 = Enum.count(items)
+    end
   end
 end
